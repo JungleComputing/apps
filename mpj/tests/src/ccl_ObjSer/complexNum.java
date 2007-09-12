7b881f72e03eafd15a527db0b9a72c63bab917fa
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2007-09-12 12:26:49 +0000

    Moved to src dir

---
+++ mpj/tests/src/ccl_ObjSer/complexNum.java
@@ -0,0 +1,10 @@
+/* $Id$ */
+
+
+package ccl_ObjSer;
+
+import java.io.*;
+class complexNum implements Serializable{
+  float realPart;
+  float imaginPart;
+}
---
+++ mpj/tests/src/pt2pt_ObjSer/test.java
@@ -0,0 +1,13 @@
+/* $Id$ */
+
+package pt2pt_ObjSer;
+
+
+import java.io.*;
+class test implements Serializable{
+
+  public int a = 112;
+  public String b = "test1";
+  public int c = 999;
+}
+
